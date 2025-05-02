d032341c7f78635143fc4721506ac4c9bf2fcd4b
mohamed khaled <mohakhaled324@gmail.com>
2025-05-02 16:09:41 +0300

    update 2

--- my-website/src/Components/Contact.jsx
+++ my-website/src/Components/Contact.jsx
@@ -1,17 +1,26 @@
-import { useEffect, useState } from 'react';
-import Lottie from 'lottie-react';
+import { useEffect, useState } from "react";
+import Lottie from "lottie-react";
+import { TbBrandLinkedinFilled } from "react-icons/tb";
+import { SlSocialGithub } from "react-icons/sl";
+import { FaFacebook } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+import { TbBrandWhatsapp } from "react-icons/tb";
 
 const Contact = () => {
   const [contactData, setContactData] = useState(null);
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
   const [loading, setLoading] = useState(false);
-  const [responseMessage, setResponseMessage] = useState('');
+  const [responseMessage, setResponseMessage] = useState("");
 
   useEffect(() => {
-    fetch('/Animations/Contact.json')
+    fetch("/Animations/Contact.json")
       .then((response) => response.json())
       .then((data) => setContactData(data))
-      .catch((error) => console.error('Error loading animation data:', error));
+      .catch((error) => console.error("Error loading animation data:", error));
   }, []);
 
   const handleChange = (e) => {
@@ -21,67 +30,158 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    setResponseMessage('');
+    setResponseMessage("");
 
     try {
-      const response = await fetch('https://send-email-crrw.onrender.com/api/sendEmail', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-      
+      const response = await fetch(
+        "https://send-email-crrw.onrender.com/api/sendEmail",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
       const data = await response.json();
-      
+
       if (response.ok) {
-        setResponseMessage('Email sent successfully!');
-        setFormData({ name: '', email: '', message: '' });
+        setResponseMessage("Email sent successfully!");
+        setFormData({ name: "", email: "", message: "" });
       } else {
-        setResponseMessage(data.error || 'Failed to send email');
+        setResponseMessage(data.error || "Failed to send email");
       }
     } catch (error) {
-      setResponseMessage('An error occurred. Please try again.');
+      setResponseMessage("An error occurred. Please try again.");
     }
     setLoading(false);
   };
 
   return (
-    <section id="contact" className="z-50 bg-gray-800 relative py-10 px-5 md:px-0">
+    <section
+      id="contact"
+      className="z-50 bg-gray-800 relative py-10 px-5 md:px-0"
+    >
       <div className="mb-16 max-w-7xl mx-auto">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="md:w-1/2 mb-8 ml-2 md:mb-0">
-            <h2 className="text-3xl font-bold mb-3 text-red-500">Get in Touch</h2>
-            <p className="mb-4 text-white/85">I'm always open to new opportunities and collaboration. Feel free to reach out!</p>
+            <h2 className="text-3xl font-bold mb-3 text-red-500">
+              Get in Touch
+            </h2>
+            <p className="mb-4 text-white/85">
+              I'm always open to new opportunities and collaboration. Feel free
+              to reach out!
+            </p>
             <div className="flex space-x-4 mb-5">
-              <a href="https://www.facebook.com/profile.php?id=100007448079879" target="_blank" className="text-foreground/60 hover:text-foreground/80">
-                <i className="text-3xl fa-brands fa-facebook text-gray-400"></i>
+              <a
+                href="https://www.facebook.com/profile.php?id=100007448079879"
+                target="_blank"
+                className="text-gray-400 hover:text-blue-600"
+              >
+                <FaFacebook size={32} />
               </a>
-              <a href="https://www.instagram.com/mohakhaled_official/" target="_blank" className="text-foreground/60 hover:text-foreground/80">
-                <i className="text-3xl fa-brands fa-instagram text-gray-400"></i>
+              <a
+                href="https://www.instagram.com/mohakhaled_official/"
+                target="_blank"
+                className="text-gray-400 hover:text-pink-600"
+              >
+                <AiFillInstagram size={32} />
               </a>
-              <a href="https://wa.me/201144877902" target="_blank" className="text-foreground/60 hover:text-foreground/80">
-                <i className="text-3xl fa-brands fa-whatsapp text-gray-400"></i>
+              <a
+                href="https://wa.me/201144877902"
+                target="_blank"
+                className="text-gray-400 hover:text-green-600"
+              >
+                <TbBrandWhatsapp size={32} />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/mo%C4%A5%C3%A3med-k%C4%A5aled-42509b322/"
+                target="_blank"
+                className="text-gray-400 hover:text-blue-500"
+              >
+                <TbBrandLinkedinFilled size={32} />
+              </a>
+              <a
+                href="https://github.com/moha-med610"
+                target="_blank"
+                className="text-gray-400 hover:text-black"
+              >
+                <SlSocialGithub size={32} />
               </a>
             </div>
-            {contactData && <Lottie animationData={contactData} className='w-[350px] mx-auto lg:w-[]500px'/>}
+            {contactData && (
+              <Lottie
+                animationData={contactData}
+                className="w-[350px] mx-auto lg:w-[]500px"
+              />
+            )}
           </div>
-          <form onSubmit={handleSubmit} className='w-full md:w-1/2 bg-gray-100 rounded-lg border border-red-300 shadow-lg shadow-red-500 p-10'>
-            <h1 className='text-gray-900 text-4xl font-bold mb-7'>Contact Me</h1>
-            <div className='mb-4'>
-              <label htmlFor="name" className='block text-sm font-medium text-gray-700'>Name</label>
-              <input type="text" id='name' value={formData.name} onChange={handleChange} required className='mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50' placeholder='Enter Your Name' />
-              
-              <label htmlFor="email" className='block text-sm font-medium text-gray-700'>Email</label>
-              <input type="email" id='email' value={formData.email} onChange={handleChange} required className='mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50' placeholder='Enter Your Email' />
-              
-              <label htmlFor="message" className='block text-sm font-medium text-gray-700'>Message</label>
-              <textarea id='message' value={formData.message} onChange={handleChange} required className='mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50' placeholder='Enter your Message' />
+          <form
+            onSubmit={handleSubmit}
+            className="w-full md:w-1/2 bg-gray-100 rounded-lg border border-red-300 shadow-lg shadow-red-500 p-10"
+          >
+            <h1 className="text-gray-900 text-4xl font-bold mb-7">
+              Contact Me
+            </h1>
+            <div className="mb-4">
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+                className="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                placeholder="Enter Your Name"
+              />
+
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                className="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                placeholder="Enter Your Email"
+              />
+
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Message
+              </label>
+              <textarea
+                id="message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+                className="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                placeholder="Enter your Message"
+              />
             </div>
-            <button type="submit" className='bg-red-500 text-white px-3 py-2 rounded-lg' disabled={loading}>
-              {loading ? 'Sending...' : 'Send Message'}
+            <button
+              type="submit"
+              className="bg-red-500 text-white px-3 py-2 rounded-lg"
+              disabled={loading}
+            >
+              {loading ? "Sending..." : "Send Message"}
             </button>
-            {responseMessage && <p className='mt-4 text-gray-900'>{responseMessage}</p>}
+            {responseMessage && (
+              <p className="mt-4 text-gray-900">{responseMessage}</p>
+            )}
           </form>
         </div>
       </div>
--- my-website/src/Components/Hero.jsx
+++ my-website/src/Components/Hero.jsx
@@ -1,33 +1,109 @@
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
+import { FaFacebook } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+import { SlSocialGithub } from "react-icons/sl";
+import { TbBrandLinkedinFilled } from "react-icons/tb";
+
 const Hero = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Mohamed Khaled"],
+      typeSpeed: 120,
+      loop: true,
+      backSpeed: 80,
+    });
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
-    <section className="relative"> 
+    <section className="relative">
       <div className="max-w-7xl mx-auto">
         <div className="flex flex-col md:flex-row items-center lg:h-[90vh] justify-between">
-            <div className="md:w-1/2 mb-8 md:mb-0 flex flex-col space-y-4 px-6 lg:mt:mt-0 mt-10">
-                <h1 className="lg:text-7xl text-4xl font-bold lg:leading-snug">Hi There, <br />
-                I,m <span className="text-red-600">Mohamed </span>Khaled</h1>
-                <p className="md:text-2xl text-xl mb-4">Full Stack Developer & Web Designer</p>
-                <p className="mb-4">I'm a passionate web developer with expertise in <span className="text-2xl text-blue-600">React</span> , <span className="text-2xl">Express js</span>, and modern web technologies. I love creating beautiful and functional website that solve real world problems.</p>
-                <a href="https://drive.google.com/file/d/1670D-YwFpUKlkUxRNFPuLm9YQUAUqDUc/view?usp=drive_link" target="_blanc">
-                <button className="bg-red-600 text-white px-3 py-2 w-max rounded-md">Download CV</button>
-              </a>
-            </div>
-            <div className="md:w-1/2 relative flex justify-center items-end">
-            <img src="/images/hero.png" alt="" className="lg:h-[90vh] h-96"/>
-            <img src="/images/js-logo.png" alt="" className="absolute w-10 top-36 left-0 rounded-full md:hidden"/>
-            <img src="/images/tailwind.png" alt="" className="absolute w-10 top-0 right-5 md:hidden rounded-full"/>
-            <img src="/images/react-logo.png" alt="" className="absolute w-10 rounded-full right-0  bottom-36 md:hidden"/>
-            </div>
+          <div className="md:w-1/2 mb-8 md:mb-0 flex flex-col space-y-4 px-6 lg:mt:mt-0 mt-10">
+            <h1 className="lg:text-7xl text-4xl font-bold lg:leading-snug">
+              Hi There, <br />
+              I,m <span className="text-red-600" ref={el} />
+            </h1>
+            <p className="md:text-2xl text-xl mb-4">
+              Full Stack Developer & Web Designer
+            </p>
+            <p className="mb-4">
+              I'm a passionate web developer with expertise in
+              <span className="text-2xl font-bold">React, </span>
+              <span className="text-2xl font-bold">Express js</span>, and modern
+              web technologies. I love creating beautiful and functional website
+              that solve real world problems.
+            </p>
+            <a
+              href="https://drive.google.com/file/d/1jjiAOGkjVIQ1bHKZnaUgvhtt5Nb602da/view?usp=drive_link"
+              target="_blanc"
+            >
+              <button className="bg-red-600 text-white px-3 py-2 w-max rounded-md">
+                Download CV
+              </button>
+            </a>
+          </div>
+          <div className="md:w-1/2 relative flex justify-center items-end">
+            <img src="/images/hero.png" alt="" className="lg:h-[90vh] h-96" />
+            <img
+              src="/images/js-logo.png"
+              alt=""
+              className="absolute w-10 top-36 left-0 rounded-full md:hidden"
+            />
+            <img
+              src="/images/tailwind.png"
+              alt=""
+              className="absolute w-10 top-0 right-5 md:hidden rounded-full"
+            />
+            <img
+              src="/images/react-logo.png"
+              alt=""
+              className="absolute w-10 rounded-full right-0  bottom-36 md:hidden"
+            />
+          </div>
         </div>
       </div>
-      <div className="absolute top-40 right-10 hidden bg-gray-100    border-collapse p-4 md:flex flex-col gap-6 rounded-full">
-        <a href="https://www.facebook.com/profile.php?id=100007448079879" target="_blank"><img src="/images/facebook.png" alt="" className="w-20 rounded-full"/></a>
-        <a href="https://www.instagram.com/mohakhaled_official/" target="_blank"><img src="/images/instagram.png" alt="" className="w-20 rounded-3xl"/></a>
-        <a href="https://x.com/Fantzia_" target="_blank"><img src="/images/twiter.png" alt="" className="w-20 rounded-2xl"/></a>
-        <a href="https://www.linkedin.com/in/mo%C4%A5%C3%A3med-k%C4%A5aled-42509b322/" target="_blank"><img src="/images/linkedin.png" alt="" className="w-20 rounded-3xl"/></a>
+      <div className="absolute top-40 right-10 hidden md:flex flex-col gap-6 bg-gray-100 p-4 rounded-full shadow-md">
+        <a
+          href="https://www.facebook.com/profile.php?id=100007448079879"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-500 hover:text-blue-600 transition-colors duration-300"
+        >
+          <FaFacebook size={70} />
+        </a>
+        <a
+          href="https://www.instagram.com/mohakhaled_official/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-500 hover:text-pink-600 transition-colors duration-300"
+        >
+          <AiFillInstagram size={70} />
+        </a>
+        <a
+          href="https://github.com/moha-med610"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-500 hover:text-black transition-colors duration-300"
+        >
+          <SlSocialGithub size={70} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/mo%C4%A5%C3%A3med-k%C4%A5aled-42509b322/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-500 hover:text-blue-500 transition-colors duration-300"
+        >
+          <TbBrandLinkedinFilled size={70} />
+        </a>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
